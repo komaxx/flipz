cee94e3adf93003f75fd67c9ff1dda2b3b9613a7
Matthias Schicker <matthias.schicker@gmail.com>
2013-06-14 18:47:34 +0200

    Initial refactoring to keep directory structure uncluttered

---
+++ client/iOS/Flip4Friends/UI/FFBoardView.m
@@ -0,0 +1,27 @@
+//
+//  FFBoardView.m
+//  Flip4Friends
+//
+//  Created by Matthias Schicker on 6/14/13.
+//  Copyright (c) 2013 FlippyFriends. All rights reserved.
+//
+
+#import "FFBoardView.h"
+
+@interface FFBoardView()
+@property (strong, nonatomic) NSArray* tiles;
+@end
+
+@implementation FFBoardView
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+
+@end
---
+++ client/iOS/Flip4Friends/UI/FFFlipTile.h
@@ -0,0 +1,13 @@
+//
+//  FFFlipTile.h
+//  Flip4Friends
+//
+//  Created by Matthias Schicker on 6/14/13.
+//  Copyright (c) 2013 FlippyFriends. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FFFlipTile : UIView
+
+@end
